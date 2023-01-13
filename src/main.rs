7cf9e9f9f01724e2976cab9e37a6604d142811c6
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-13 03:58:00 +0000

    Get commits using regex pattern

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,10 @@ fn main() {
         .arg("--oneline")
         .output()
         .expect("failed to execute process");
+    // regex pattern to match the commit hash and get message
+    let re = Regex::new(r"([a-f0-9]{7}) (.*)").unwrap();
+    // iterate over the git logs
+    for cap in re.captures_iter(&String::from_utf8_lossy(&output.stdout)) {
+        println!("{}: {}", &cap[1], &cap[2]);
+    }
 }
